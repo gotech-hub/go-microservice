495a1eeb686d572a17b1227b3684dfc9067ad6be
dungnguyen <dungnt23@vng.com.vn>
2025-07-03 17:14:37 +0700

    feat: Add Swagger documentation annotations for Social Service API

--- app/main.go
+++ app/main.go
@@ -20,6 +20,21 @@ import (
 	logger "go-source/pkg/log"
 )
 
+// @title Social Service API
+// @version 1.0
+// @description This is a social service API server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath
+
 func main() {
 	// Initialize logging system with service name
 	logger.InitLog("go-source")
